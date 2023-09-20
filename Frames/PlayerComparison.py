f1a9132b8bac7c5dd0001e96406f038afbbc3364
Logan <lcheyne@rogers.com>
2023-09-20 09:42:01 -0300

    Got query working kind of removal still needs work

--- Frames/PlayerComparison.py
+++ Frames/PlayerComparison.py
@@ -6,7 +6,7 @@ from tkinter import Listbox, ttk, messagebox
 class PlayerComparison(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.equalities = ['is equal to', 'is greater or equal to', 'is less or equal to', 'is less then', ' is greater then',  'is not']
+        self.equalities = ['is equal to', 'is greater or equal to', 'is less or equal to', 'is less then', 'is greater then',  'is not']
         self.pOptions = ['Add', 'Remove']
         self.var_pOptions = tk.StringVar()
         self.var_equalities = tk.StringVar()
@@ -25,8 +25,8 @@ class PlayerComparison(tk.Frame):
         self.avg_bool_var = tk.BooleanVar()
         self.ltg.set("False")
         self.list_box = Listbox(self, height=6, listvariable=self.saved_var)
-        self.list_box.configure(width=int(controller.winfo_width()/2.7))
-        self.list_box.grid(column=0, row=4, columnspan=6, pady=3, padx=3)
+        self.list_box.configure(width=int(controller.winfo_width()/3))
+        self.list_box.grid(column=0, row=5, columnspan=3, pady=3, padx=3)
         self.label = ttk.Label(self, text="CSV", font=self.controller.LARGEFONT)
         self.label.grid(columnspan=6, column=0, row=0, padx=30, pady=10)
         #button0 = ttk.Button(self, text="-",
@@ -34,19 +34,19 @@ class PlayerComparison(tk.Frame):
         #button0.grid(row=3, column=3, padx=10, pady=10)
         self.button1 = ttk.Button(self, text="Show table",
                                   command=lambda: self.show_selected_table())
-        self.button1.grid(row=2, column=0, columnspan=2, rowspan=2, padx=10, pady=10)
+        self.button1.grid(row=3, column=2, padx=10, pady=10)
         button2 = ttk.Button(self,
                              text="Ok",
                              command=lambda: self.queryPlayers())
-        button2.grid(row=2, column=4, columnspan=2, padx=10, pady=10)
+        button2.grid(row=2, column=2, padx=5, pady=10)
         dropChoice = tk.OptionMenu(self, self.var_pOptions, *self.pOptions)
         dropChoice.grid(row=1,column=0,pady=10, padx=5)
         l = ttk.Label(self, text="all players where stat:")
         self.key = ttk.Entry(self, textvariable=self.query)
         l.grid(row=1, column=1, padx=5, pady=10)
-        self.key.grid(row=1, column=4, columnspan=2, padx=5, pady=10)
+        self.key.grid(row=2, column=1, padx=5, pady=10)
         eDrop = tk.OptionMenu(self, self.var_equalities, *self.equalities)
-        eDrop.grid(row=1, column=3, padx=5, pady=10)
+        eDrop.grid(row=2, column=0, padx=5, pady=10)
 
     def load_sheet(self, filename, sheet):
         self.SHEET = sheet
@@ -61,16 +61,66 @@ class PlayerComparison(tk.Frame):
 
     def queryPlayers(self):
         players = self.SHEET.get_col_data(self.cat_c.get())
-        for num in players.keys():
-            player = players.get(num)
-            if player == self.query.get():
-                self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
-                self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name') + "\t\tCategory "
-                                                                                                     "Found: " +
-                                         str(self.cat_c.get()))
-        self.saved_var.set(self.tables_saved)
-        self.list_box.configure(listvariable=self.saved_var)
-
+        if self.var_pOptions.get()=='Add':
+            for num in players.keys():
+                player = players.get(num)
+                match self.var_equalities.get():
+                    case 'is equal to':
+                        if player == self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+                    case 'is greater or equal to':
+                        if player >= self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+                    case 'is less or equal to':
+                        if player <= self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+                    case 'is less then':
+                        if player < self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+                    case 'is greater then':
+                        if player > self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+                    case 'is not':
+                        if player != self.query.get():
+                            self.PLAYERS[num] = self.SHEET.getPlayer(num).get_stats(self.SHEET.cols)
+                            self.tables_saved.append("Player : " + self.SHEET.getPlayer(num).get_stats('Name'))
+            self.saved_var.set(self.tables_saved)
+            self.list_box.configure(listvariable=self.saved_var)
+        elif self.var_pOptions.get()=='Remove':
+            for num in self.PLAYERS.keys():
+                player = players.get(num)
+                match self.var_equalities.get():
+                    case 'is equal to':
+                        if player == self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+                    case 'is greater or equal to':
+                        if player >= self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+                    case 'is less or equal to':
+                        if player <= self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+                    case 'is less then':
+                        if player < self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+                    case 'is greater then':
+                        if player < self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+                    case 'is not':
+                        if player != self.query.get():
+                            self.tables_saved.pop(self.tables_saved.index(self.SHEET.getPlayer(num).get_stats('Name')))
+                            self.PLAYERS.pop(num)
+            self.saved_var.set(self.tables_saved)
+            self.list_box.configure(listvariable=self.saved_var)
     def show_selected_table(self):
         if len(self.PLAYERS) < 1:
             messagebox.showwarning('Table Display Error', 'Please add ' + str(2-len(self.PLAYERS)) + " more players")
@@ -81,11 +131,6 @@ class PlayerComparison(tk.Frame):
             selectedPlayers = list(self.PLAYERS.keys())
             self.controller.player_compare_data_display_tree(stats, selectedPlayers)
 
-    def remove_selected(self):
-        self.tables_saved.pop(self.get_selected_index())
-        self.controller.SELECTED.pop(self.get_selected_index())
-        self.saved_var.set(self.tables_saved)
-        self.list_box.configure(listvariable=self.saved_var)
 
     def get_selected_index(self):
         i = 0
